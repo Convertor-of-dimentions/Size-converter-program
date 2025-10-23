3d2ecc9bc4a7e5bafe0143f78ef6fcfea182542e
AndyKor666 <andrey.korotkov1304@gmail.com>
2025-10-23 14:15:59 +0300

    Main logic function

--- Size converter/Size_converter.py
+++ Size converter/Size_converter.py
@@ -9,3 +9,33 @@ def mm_to_cm(mm):
 
 def cm_to_mm(cm):
     return cm * 10
+
+def main():
+   
+    while True:
+     
+        if not choice.isdigit() or int(choice) not in range(0, 5):
+       
+            continue
+
+        choice = int(choice)
+
+        if choice == 0:
+           
+            break
+        try:
+            if value <= 0:
+          
+                continue
+
+        except ValueError:
+            
+            continue
+
+        if choice == 1:
+            
+        elif choice == 2:
+          
+        elif choice == 3:
+          
+        elif choice == 4:
